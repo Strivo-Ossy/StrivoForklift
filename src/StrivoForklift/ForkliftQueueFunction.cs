11727ef6190452071505c0483a1099ed9d871916
Ossy Drakeneld <ossy.drakeneld@strivo.net>
2026-03-20 11:11:25 +0100

    fix queue trigger

--- src/StrivoForklift/ForkliftQueueFunction.cs
+++ src/StrivoForklift/ForkliftQueueFunction.cs
@@ -25,7 +25,7 @@ public class ForkliftQueueFunction
 
     [Function(nameof(ForkliftQueueFunction))]
     public async Task Run(
-        [QueueTrigger("consumethis", Connection = "StorageQueue")] string rawMessage)
+        [QueueTrigger("demo2-queue-ossy", Connection = "StorageQueue")] string rawMessage)
     {
         _logger.LogInformation("Dequeued raw message: {RawMessage}", rawMessage);
 
